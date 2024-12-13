e2d46b58304861857901b4e7c5d1d058a4bde92c
Oleh <oleh-edu@users.noreply.github.com>
2024-12-14 01:41:59 +0200

    fix: add configs.py

---
+++ configs.py
@@ -0,0 +1,9 @@
+kafka_config = {
+    "bootstrap_servers": ['<set_your_own>'],
+    "username": '<set_your_own>',
+    "password": '<set_your_own>',
+    "security_protocol": 'SASL_PLAINTEXT',
+    "sasl_mechanism": 'PLAIN'
+
+}
+
